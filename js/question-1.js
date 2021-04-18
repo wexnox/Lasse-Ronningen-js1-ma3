160d06ba298b26d62539c80ad3d8f87f119efb48
wexnox <lasseronnlie@hotmail.com>
2021-04-18 23:06:25 +0200

    Initial commit

---
+++ js/question-1.js
@@ -0,0 +1,12 @@
+// Question 1
+
+// Convert the function below to an arrow function:
+// function division(a, b) {
+//   return a % b;
+// }
+
+const division = (a, b) => a % b;
+
+const result = division(10, 3);
+
+console.log(result);
---
+++ js/question-2.js
@@ -0,0 +1,42 @@
+// Question 2
+// Make a call to the following API endpoint:
+// https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating
+
+// Loop through the results and display the following properties in HTML, but only for the first eight results:
+// name
+// rating
+// number of tags (not the tag details, just the amount of tags)
+
+// The styling for this assignment is not important but loading indicator should be displayed while the API call is in progress.
+
+// Be sure to handle any potential errors in the code.
+
+// Be sure to arrange all file types appropriately, consult the repos from the lessons for examples.
+const url =
+  "https://api.rawg.io/api/games?key=9f2ddd85529c480fbb86c3450d82d4b2&dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFix = proxy + url;
+const resultsContainer = document.querySelector(".results");
+
+async function getGames() {
+  try {
+    const response = await fetch(corsFix);
+
+    const all = await response.json();
+
+    const game = all.results;
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < game.length; i++) {
+      if (i === 8) {
+        break;
+      }
+      resultsContainer.innerHTML += `<div class="result">${game[i].name}</div>`;
+    }
+  } catch (error) {
+    console.log(error);
+    // resultsContainer.innerHTML = error;
+  }
+}
+getGames();
